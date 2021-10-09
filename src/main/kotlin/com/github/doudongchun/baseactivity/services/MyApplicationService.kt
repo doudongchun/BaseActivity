2d17d204532766a49a3ca51cc14124aa0c79cbe4
GitHub Action <action@github.com>
2021-10-09 03:57:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/doudongchun/baseactivity/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/doudongchun/baseactivity/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.doudongchun.baseactivity.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.doudongchun.baseactivity.MyBundle
 
 class MyApplicationService {
 
